817fb40945f2de5e49488178e6e871e014f36a27
Arif's Zone <arifulislam.java.c@gmail.com>
2023-09-02 04:01:17 -0700

    master

--- lib/chatmain.dart
+++ lib/chatmain.dart
@@ -270,11 +270,19 @@ Future<void> fetchLabelDataBySubCategory23() async {
   }
 }
  */
+//updated
+/*
 
+import 'dart:convert';
 
+import 'package:chatfeatures/chatModel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-
-
+import 'package:chatfeatures/chatCheck.dart';
+import 'package:chatfeatures/chatModel.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -287,6 +295,14 @@ class chatmain extends StatefulWidget {
 
 String itemmm = "A203";
 class _chatmainState extends State<chatmain> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    setState(() {
+      int_loadData();
+    });
+    super.initState();
+  }
   void showCustomBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
@@ -478,6 +494,10 @@ class _chatmainState extends State<chatmain> {
     );
 
   }
+
+  void int_loadData() async{
+     await fetchLabelDataBySubCategory23();
+  }
 }
 List<ChatMessage> messages = [
   ChatMessage(messageContent: "Hello, Will", messageType: "receiver",servertime: "122"),
@@ -493,3 +513,348 @@ class ChatMessage{
   String servertime;
   ChatMessage({required this.messageContent, required this.messageType, required this.servertime});
 }
+List<ChatModel> messages22 = [];
+Future<void> fetchLabelDataBySubCategory23() async {
+  final url = 'http://web-api-tht-env.eba-kcaa52ff.us-east-1.elasticbeanstalk.com/api/dev/messages/4';
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    final List<dynamic> labelDataList = json.decode(response.body);
+    for (var item in labelDataList) {
+      final chatModel = ChatModel.fromJson(item);
+      messages22.add(chatModel);
+    }
+
+
+    print(messages22);
+  } else {
+    print(" Status code: ${response.statusCode}");
+    print("Response body: ${response.body}");
+  }
+}
+ */
+
+
+import 'dart:convert';
+
+import 'package:chatfeatures/chatModel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:chatfeatures/chatCheck.dart';
+import 'package:chatfeatures/chatModel.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class chatmain extends StatefulWidget {
+  const chatmain({super.key});
+
+  @override
+  State<chatmain> createState() => _chatmainState();
+}
+
+String itemmm = "A203";
+class _chatmainState extends State<chatmain> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    setState(() {
+      int_loadData();
+    });
+    super.initState();
+  }
+  void showCustomBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildRow(context, "Select Image", Icons.image, "Select Video", Icons.video_library),
+              _buildRow(context, "Voice", Icons.mic, "Select Document", Icons.file_copy),
+            ],
+          ),
+        );
+      },
+    );
+  }
+  Widget _buildRow(BuildContext context, String text1, IconData icon1, String text2, IconData icon2) {
+    return Row(
+      children: [
+        _buildOption(context, text1, icon1),
+        _buildOption(context, text2, icon2),
+      ],
+    );
+  }
+  Widget _buildOption(BuildContext context, String text, IconData icon) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {
+          // Handle option selection here
+          if(text=="Select Image")
+          {
+            print(text);
+          }
+          else if(text=="Select Video")
+          {
+            print(text);
+          }
+          else if(text=="Voice")
+          {
+            print(text);
+          }
+          else if(text=="Select Document")
+          {
+            print(text);
+          }
+
+          Navigator.pop(context); // Close the BottomSheetDialog
+        },
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Icon(icon, size: 48),
+              SizedBox(height: 8),
+              Text(text, textAlign: TextAlign.center),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  TextEditingController textMessage= new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.only(right: 16),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back,color: Colors.black,),
+                  ),
+                  SizedBox(width: 2,),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage("<https://randomuser.me/api/portraits/men/5.jpg>"),
+                    maxRadius: 20,
+                  ),
+                  SizedBox(width: 12,),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Kriss Benwat",style: TextStyle( fontSize: 16 ,fontWeight: FontWeight.w600),),
+                        SizedBox(height: 6,),
+                        Text("Online",style: TextStyle(color: Colors.grey.shade600, fontSize: 13),),
+                      ],
+                    ),
+                  ),
+                  Icon(Icons.settings,color: Colors.black54,),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ListView.builder(
+                itemCount: messages22.length,
+                shrinkWrap: true,
+                padding: EdgeInsets.only(top: 10,bottom: 10),
+                physics: NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index){
+                  return Container(
+                    padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+                    child: Align(
+                      alignment: (messages22[index].msgType == "receiver"?Alignment.topLeft:Alignment.topRight),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: (messages22[index].msgType  == "receiver"?Colors.grey.shade200:Colors.blue[200]),
+                        ),
+                        padding: EdgeInsets.all(16),
+                        child: Text(messages22[index].message.toString(), style: TextStyle(fontSize: 15),),
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  padding: EdgeInsets.only(left: 10,bottom: 10,top: 10),
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: (){
+                          print("getFiles");
+                          showCustomBottomSheet(context);
+                        },
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Icon(Icons.add, color: Colors.white, size: 20, ),
+                        ),
+                      ),
+                      SizedBox(width: 15,),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                              hintText: "Write message...",
+                              hintStyle: TextStyle(color: Colors.black54),
+                              border: InputBorder.none
+                          ),
+                          controller: textMessage,
+                        ),
+                      ),
+                      SizedBox(width: 15,),
+                      FloatingActionButton(
+                        onPressed: ()async{
+                          String message_tobesend= textMessage.text.toString();
+                          print(message_tobesend);
+
+                          if(message_tobesend.toString().isEmpty)
+                          {
+                            print("Empty");
+                            // await sendChatMessage2222(message_tobesend.toString());
+                          }
+                          else
+                          {
+                            print("NON Empty");
+                            await sendChatMessage2222(message_tobesend.toString());
+                            textMessage.text = "";
+                          }
+
+                        },
+                        child: Icon(Icons.send,color: Colors.white,size: 18,),
+                        backgroundColor: Colors.blue,
+                        elevation: 0,
+                      ),
+                    ],
+
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+        ),
+      ),
+    );
+
+  }
+  Future<void> sendChatMessage2222(String sendmessage) async {
+    final url = 'http://web-api-tht-env.eba-kcaa52ff.us-east-1.elasticbeanstalk.com/api/dev/messages'; // Replace with your API URL
+
+    final Map<String, dynamic> messageData = {
+      "chatId": 4,
+      "sentBy": "3",
+      "sentTo": "2",
+      "message": sendmessage,
+      "msgType": "sender",
+      "timestmp": "2023-08-31T08:11:05.814+00:00"
+    };
+
+    final headers = {
+      'Content-Type': 'application/json', // Set the content type to JSON
+    };
+
+    final String jsonBody = json.encode(messageData);
+
+    final response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: jsonBody,
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseBody = json.decode(response.body);
+      print('Message sent successfully.');
+      final chatModel = ChatModel.fromJson(responseBody);
+
+      // Add the sent message to the list and update the UI
+      setState(() {
+        messages22.add(chatModel);
+      });
+    }
+    else if (response.statusCode == 201) {
+      final Map<String, dynamic> responseBody = json.decode(response.body);
+
+      print('Message sent successfully.');
+      final chatModel = ChatModel.fromJson(responseBody);
+
+      // Add the sent message to the list and update the UI
+      setState(() {
+        messages22.add(chatModel);
+      });
+    }else {
+      print('Failed to send message. Status code: ${response.statusCode}');
+      print('Response body: ${response.body}');
+    }
+  }
+  void int_loadData() async{
+    await fetchLabelDataBySubCategory23();
+  }
+  List<ChatModel> messages22 = [];
+  Future<void> fetchLabelDataBySubCategory23() async {
+    final url =
+        'http://web-api-tht-env.eba-kcaa52ff.us-east-1.elasticbeanstalk.com/api/dev/messages/4';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> labelDataList = json.decode(response.body);
+      setState(() {
+        messages22.clear(); // Clear existing messages
+        for (var item in labelDataList) {
+          final chatModel = ChatModel.fromJson(item);
+          messages22.add(chatModel);
+        }
+      });
+      print(messages22);
+    } else {
+      print(" Status code: ${response.statusCode}");
+      print("Response body: ${response.body}");
+    }
+  }
+}
+/*
+List<ChatMessage> messages = [
+  ChatMessage(messageContent: "Hello, Will", messageType: "receiver",servertime: "122"),
+  ChatMessage(messageContent: "How have you been?", messageType: "receiver",servertime: "122"),
+  ChatMessage(messageContent: "Hey Kriss, I am doing fine dude. wbu?", messageType: "sender",servertime: "122"),
+  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver",servertime: "122"),
+  ChatMessage(messageContent: "Is there any thing wrong?", messageType: "sender",servertime: "122"),
+];
+
+class ChatMessage{
+  String messageContent;
+  String messageType;
+  String servertime;
+  ChatMessage({required this.messageContent, required this.messageType, required this.servertime});
+}
+ */
+
